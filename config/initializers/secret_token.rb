bf3fcf4a3562578ee35a9533265f056d8fae23a9
ash28 <ashlesh.iitb@gmail.com>
2014-11-11 13:45:38 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kadak::Application.config.secret_key_base = '7e96fed8920a79765c54e9a1a814d13965b8498a92a587051ba898f5ebfddc62337dbbc5bba269b95590111c1b65a8e48ce960cd1b15871f726f2d85323aa189'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kadak::Application.config.session_store :cookie_store, key: '_kadak_session'
